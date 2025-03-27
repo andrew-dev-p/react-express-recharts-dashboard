ddd7ab3e90da16f18c294d8e7a3c1fc2d857c6db
andrew-dev-p <popov.andrew.dev@gmail.com>
2025-03-27 18:01:22 +0200

    Add Redux store setup and API slice for KPI data fetching

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,24 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@/index.css";
 import App from "@/App";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { api } from "./state/api";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
+});
+
+setupListeners(store.dispatch);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
