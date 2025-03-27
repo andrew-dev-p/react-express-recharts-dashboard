5f44f5a22e0d8a300adcc5c6a892b6d1afdbf61f
andrew-dev-p <popov.andrew.dev@gmail.com>
2025-03-27 17:36:54 +0200

    Add Dashboard component and integrate it into the main application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { useMemo } from "react";
 import { themeSettings } from "@/theme";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "@/components/Navbar";
+import Dashboard from "@/pages/Dashboard";
 
 const App = () => {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -15,7 +16,7 @@ const App = () => {
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
             <Navbar />
             <Routes>
-              <Route path="/" element={<div>Home</div>} />
+              <Route path="/" element={<Dashboard />} />
               <Route path="/predictions" element={<div>Predictions</div>} />
             </Routes>
           </Box>
--- client/src/expanded-theme.ts
+++ client/src/expanded-theme.ts
@@ -1,12 +1,18 @@
 // eslint-disable-next-line
-import type { Palette, PaletteColor } from "@mui/material";
+import type { Palette, PaletteColor, TypeBackground } from "@mui/material";
 
 declare module "@mui/material" {
   interface PaletteColor {
     [key: number]: string;
   }
 
+  interface TypeBackground {
+    light: string;
+    main: string;
+  }
+
   interface Palette {
     tertiary: PaletteColor;
+    background: TypeBackground;
   }
 }
