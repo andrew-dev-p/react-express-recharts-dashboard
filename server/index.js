5b70a180f246c7b79f72b2c814c3f7d0d7b5507c
andrew-dev-p <popov.andrew.dev@gmail.com>
2025-04-01 13:30:04 +0300

    Vercel configuration

--- server/index.js
+++ server/index.js
@@ -1,19 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-import kpiRoutes from "./routes/kpi.js";
-import productRoutes from "./routes/product.js";
-import transactionRoutes from "./routes/transaction.js";
-import KPI from "./models/KPI.js";
-import Product from "./models/Product.js";
-import Transaction from "./models/Transaction.js";
-import { kpis, products, transactions } from "./data/data.js";
+
+import kpiRoutes from "../routes/kpi.js";
+import productRoutes from "../routes/product.js";
+import transactionRoutes from "../routes/transaction.js";
 
 dotenv.config();
+
 const app = express();
 app.use(express.json());
 app.use(helmet());
@@ -27,20 +25,24 @@ app.use("/kpi", kpiRoutes);
 app.use("/product", productRoutes);
 app.use("/transaction", transactionRoutes);
 
-const PORT = process.env.PORT || 9000;
+let isConnected = false;
+async function connectDB() {
+  if (isConnected) return;
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDB Connected");
+    isConnected = true;
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error);
+  }
+}
 
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(async () => {
-    app.listen(PORT, () => {
-      console.log(`Running on port ${PORT}`);
-    });
+export default async function handler(req, res) {
+  await connectDB();
+  return app(req, res);
+}
 
-    // await mongoose.connection.db.dropDatabase();
-    // KPI.insertMany(kpis);
-    // Product.insertMany(products);
-    // Transaction.insertMany(transactions);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+// await mongoose.connection.db.dropDatabase();
+// KPI.insertMany(kpis);
+// Product.insertMany(products);
+// Transaction.insertMany(transactions);
