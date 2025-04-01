169a4ff3ebe99567740b33e529a925de959e8a7e
andrew-dev-p <popov.andrew.dev@gmail.com>
2025-04-01 14:44:34 +0300

    Refactor SummaryData component to simplify text content

--- client/src/components/SummaryData.tsx
+++ client/src/components/SummaryData.tsx
@@ -21,17 +21,7 @@ const SummaryData = () => {
         />
         <Typography margin="0.5rem 0" variant="h6">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, error?
-          Molestiae, voluptas. Voluptatum quis dolore ullam neque reiciendis,
-          asperiores at quasi, consectetur quam provident cumque fugiat maiores
-          facilis qui enim cupiditate ab ratione minima aperiam dignissimos.
-          Numquam consectetur quibusdam quae praesentium explicabo? Nobis quo
-          cupiditate ipsam deleniti, esse quae nemo, odit dicta necessitatibus
-          ab modi facere, asperiores sit minus? Aperiam facere illo voluptatibus
-          dolores. Quia repellendus aliquam consectetur voluptatibus amet ipsa
-          ut eligendi deserunt dicta qui sequi dolorem molestias accusamus harum
-          provident facilis ab iure, voluptate officiis recusandae minima. Rerum
-          adipisci nostrum deleniti officiis natus perspiciatis quo ullam animi
-          neque!
+          Molestiae, voluptas.
         </Typography>
       </Box>
     </>
